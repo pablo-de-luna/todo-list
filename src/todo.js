15a81a3d0bbb6cc53cfd239be88f0bc0359331d9
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-08-18 17:14:54 -0600

    Add class to create projects
    Added functions to push projects to a projects array and a to push
    the new todos to project array

--- src/todo.js
+++ src/todo.js
@@ -1,25 +1,43 @@
 "use strict"
 
-const todoList = [];
+const projects = [];
+
+class Project {
+  constructor(name) {
+    this.name = name;
+  }
+
+  todos = [];
+}
 
 class Todo {
-  priority = 1;
-  
-  constructor(title, description, dueDate) {
+  constructor(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
+    this.priority = priority;
   }
 
-  setPriority(value) {
-    this.priority = value;
-  }
+  id = crypto.randomUUID();
 }
 
-const addTodoToList = (todo, list) => {
-  list.push(todo);
+function createProject(name) {
+  projects.push(new Project(name));
+};
+
+function createTodo(todo, projectName) {
+  const project = projects.find(project => project.name === projectName);
+
+  project.todos.push(todo);
 }
 
-addTodoToList(new Todo("Work", "work on project", "Today"), todoList);
+createProject(("defaultProject"));
+createProject(("Work"));
+createTodo(new Todo("Work", "work on project", "Today", "high"), "Work");
+createTodo(new Todo("Dance", "Dance all night", "Today", "low"), "defaultProject");
 
-console.log(todoList)
+//Log projects list
+console.log(projects)
+
+//Log project todos
+console.log(projects.find(project => project.name === "Work").todos);
