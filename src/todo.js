c98fae7f78e394f01d59e4b0a63d35f0343b105d
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-08-20 18:50:16 -0600

    Refactor two functions in todo.js
    They are not coupled to the main list anymore and can take future lists.

--- src/todo.js
+++ src/todo.js
@@ -1,6 +1,8 @@
 "use strict"
 
-const todosList = [];
+// Hierarchy: List > Projects > Todos
+
+const mainList = [];
 
 class Todo {
   constructor(title, description, dueDate, priority, project) {
@@ -12,29 +14,29 @@ class Todo {
   }
 }
 
-const addNewTodo = (newTodoInstance) => { todosList.push(newTodoInstance) };
+const addTodoToList = (todo, list = mainList) => { list.push(todo) };
 
-const getTodoListFromProject = (projectName) => {
-  const projectTodoList = todosList.filter(todo => todo.project === projectName);
+const getProjectTodos = (projectName, list = mainList) => {
+  const projectTodoList = mainList.filter(todo => todo.project === projectName);
 
   return projectTodoList;
 };
 
 // Test todos
-// addNewTodo(new Todo("Work", "work all day", "Today", "normal", "Work"));
-// addNewTodo(new Todo("Meeting", "team sync", "Tomorrow", "high", "Work"));
-// addNewTodo(new Todo("Grocery", "buy milk and bread", "Tomorrow", "normal", "Home"));
-// addNewTodo(new Todo("Code review", "review PR", "Today", "high", "Work"));
-// addNewTodo(new Todo("Laundry", "wash clothes", "Tomorrow", "low", "Home"));
-// addNewTodo(new Todo("Lunch", "meal prep", "Today", "normal", "Home"));
-// addNewTodo(new Todo("Report", "weekly report", "Friday", "high", "Work"));
-// addNewTodo(new Todo("Exercise", "gym session", "Today", "normal", "Default"));
-// addNewTodo(new Todo("Clean", "tidy up", "Saturday", "low", "Home"));
-// addNewTodo(new Todo("Deploy", "release to prod", "Today", "high", "Work"));
-// addNewTodo(new Todo("Reading", "finish chapter", "This week", "low", "Default"));
+addTodoToList(new Todo("Work", "work all day", "Today", "normal", "Work"));
+addTodoToList(new Todo("Meeting", "team sync", "Tomorrow", "high", "Work"));
+addTodoToList(new Todo("Grocery", "buy milk and bread", "Tomorrow", "normal", "Home"));
+addTodoToList(new Todo("Code review", "review PR", "Today", "high", "Work"));
+addTodoToList(new Todo("Laundry", "wash clothes", "Tomorrow", "low", "Home"));
+addTodoToList(new Todo("Lunch", "meal prep", "Today", "normal", "Home"));
+addTodoToList(new Todo("Report", "weekly report", "Friday", "high", "Work"));
+addTodoToList(new Todo("Exercise", "gym session", "Today", "normal", "Default"));
+addTodoToList(new Todo("Clean", "tidy up", "Saturday", "low", "Home"));
+addTodoToList(new Todo("Deploy", "release to prod", "Today", "high", "Work"));
+addTodoToList(new Todo("Reading", "finish chapter", "This week", "low", "Default"));
 
-// console.table(todosList);
+console.table(mainList);
 
-// console.table(getTodoListFromProject("Default"));
-// console.table(getTodoListFromProject("Work"));
-// console.table(getTodoListFromProject("Home"));
+console.table(getProjectTodos("Default"));
+console.table(getProjectTodos("Work"));
+console.table(getProjectTodos("Home"));
