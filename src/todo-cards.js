7b8717deb27a42c1f51ba919314045058d767c94
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-09-01 16:50:11 -0600

    Add function that creates todo card in DOM

--- src/todo-cards.js
+++ src/todo-cards.js
@@ -2,15 +2,47 @@
 
 import { defaultList } from "./todos.js"
 
+const cardsContainer = document.querySelector("#cards-container");
+
 // CREATE Card
   // Title
+  // Category
+  // Date
+  // Status checkbox
+  // Priority tag
 
 // EXPAND Card
 // EDIT Card
 // DELETE Card
 
-const createTodoCard = () => {
-  console.table(defaultList.todos)
+const createTodoCard = (todo) => {
+  const card = document.createElement("div");
+  card.className = "todo-card";
+  card.classList.add(`priority-${todo.priority}`);
+  card.dataset.id = todo.id;
+
+  const statusCheckbox = document.createElement("input");
+  statusCheckbox.setAttribute("type", "checkbox")
+
+  const title = document.createElement("div");
+  title.textContent = todo.title;
+
+  const category = document.createElement("div");
+  category.textContent = todo.category;
+
+  const dueDate = document.createElement("div");
+  dueDate.textContent = todo.dueDate;
+
+  [statusCheckbox, title, category, dueDate].forEach(element => {
+    card.appendChild(element);
+  });
+  
+  cardsContainer.appendChild(card);
 }
 
-createTodoCard();
+const createTodoCardsFromList = (list) => { 
+  defaultList.todos.forEach(todo => createTodoCard(todo));
+};
+createTodoCardsFromList();
+
+
